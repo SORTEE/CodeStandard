72dbe5441fa147dee879f6a1954a68b403dc039a
Saoirse Kelleher <saoirse.kelleher@student.unimelb.edu.au>
2025-09-02 16:40:36 +1000

    renamed objects to align with tidyverse style guide

--- 05_Organization_Structure/Scripts/1a_CatFoodExp2021_analysis_fitness.R
+++ 05_Organization_Structure/Scripts/1a_CatFoodExp2021_analysis_fitness.R
@@ -24,68 +24,72 @@ library(lme4)
 library(lmerTest)
 library(Rmisc)
 
+
 # Load data -----------------------------------------------------------------------------------
-d <- read.csv("Data/CatFood2021_deposit.csv")
-head(d)
+cat_food_data <- read.csv("Data/CatFood2021_deposit.csv")
+head(cat_food_data)
 
 # renaming TubeID as MotherID to match the terminology used in the Statistical section of the paper
-d <- rename(d, MotherID = TubeID)
+cat_food_data <- rename(cat_food_data,
+                        MotherID = TubeID)
 
 
 # Data summary -------------------------------------------------------------------------------
 
-length(unique(d$MotherID)) # should be 22 mothers
-table(d$Treatment) # photoperiod and mismatch treatment coded in one variable
+length(unique(cat_food_data$MotherID)) # should be 22 mothers
+table(cat_food_data$Treatment) # photoperiod and mismatch treatment coded in one variable
 
 # N per Area
-table(d[!duplicated(d$MotherID), "AreaShortName"])
+table(cat_food_data[!duplicated(cat_food_data$MotherID), "AreaShortName"])
 
 
 # Fitness curve -------------------------------------------------------------------------------
 # RQ1: What are the fitness consequences of day to day timing (a)synchrony with budburst? ####
 
 # Survival data ####
-d_surv <- d %>% mutate(PhotoTreat=gsub("(\\w+)Day.+","\\1",Treatment), MismTreat=gsub("\\w+(Day.+)","\\1",Treatment)) %>% 
+survival_data <- cat_food_data %>% 
+  mutate(PhotoTreat=gsub("(\\w+)Day.+","\\1",Treatment), 
+         MismTreat=gsub("\\w+(Day.+)","\\1",Treatment)) %>% 
   select(MotherID, Treatment, PhotoTreat, MismTreat, CaterpillarID, DeadAprilDay, PupationAprilDay) %>%
   pivot_longer(cols=c(DeadAprilDay, PupationAprilDay), names_to="Info", values_to="TimeOfEvent") %>%
   filter(!is.na(TimeOfEvent)) %>%
   mutate(Event=ifelse(Info=="DeadAprilDay", 1, 0), Treatment=as.factor(Treatment), PhotoTreat=as.factor(ifelse(PhotoTreat=="Chang", "Changing", "Constant")), MismTreatf=as.factor(MismTreat), MotherID=as.factor(MotherID)) %>%
   mutate(Treatment=factor(Treatment, levels=c("ChangDay-4", "ChangDay-3", "ChangDay-2", "ChangDay-1", "ChangDay0", "ChangDay+1", "ChangDay+2", "ChangDay+3", "ChangDay+4",
                                               "ChangDay+5",  "ConstDay-4", "ConstDay-2", "ConstDay0", "ConstDay+2", "ConstDay+4")), 
-    MismTreatf=factor(MismTreat, levels=c("Day-4", "Day-3", "Day-2", "Day-1", "Day0", "Day+1", "Day+2", "Day+3", "Day+4", "Day+5")),
-    MismTreat=as.numeric(gsub("Day(.+)","\\1",MismTreat))) %>%
+         MismTreatf=factor(MismTreat, levels=c("Day-4", "Day-3", "Day-2", "Day-1", "Day0", "Day+1", "Day+2", "Day+3", "Day+4", "Day+5")),
+         MismTreat=as.numeric(gsub("Day(.+)","\\1",MismTreat))) %>%
   mutate(MismTreat1=MismTreat+5, # no negatives to be able to fit squared term
          MismTreat2=(MismTreat+5)^2) # squared term to add in model
 # Vidisha coded it as TimeOfEvent=DeadAprilDay or PupationAprilDay, with event=Died or Survived
-head(d_surv)
-str(d_surv)
-table(d_surv$MismTreat2)
+head(survival_data)
+str(survival_data)
+table(survival_data$MismTreat2)
 
-length(unique(d_surv$CaterpillarID)) # should be 976
+length(unique(survival_data$CaterpillarID)) # should be 976
 
 
 # Survival analysis ---------------------------------------------------------------------------
 
-levels(d_surv$Treatment)
-levels(d_surv$PhotoTreat)
-levels(d_surv$MismTreatf) # as factor or not? Marcel thinks not ####
-levels(d_surv$MotherID)
-table(d_surv$TimeOfEvent)
-table(d_surv$Event) # this variable corresponds to "survival" as defined in the paper (e.g., the response variable in the first binomial mixed-effect model)
+levels(survival_data$Treatment)
+levels(survival_data$PhotoTreat)
+levels(survival_data$MismTreatf) # as factor or not? Marcel thinks not ####
+levels(survival_data$MotherID)
+table(survival_data$TimeOfEvent)
+table(survival_data$Event) # this variable corresponds to "survival" as defined in the paper (e.g., the response variable in the first binomial mixed-effect model)
 
 # Visualize survival probabilities ####
-head(d_surv)
+head(survival_data)
 
-surv_probs <- aggregate(Event~MismTreat + PhotoTreat + MotherID, d_surv, sum) # per mother
-surv_probs$samplesize <- aggregate(Info~MismTreat + PhotoTreat + MotherID, d_surv, length)$Info
-surv_probs$probs <- 100 - (surv_probs$Event/surv_probs$samplesize*100) # event = death
-head(surv_probs)
+survival_probs <- aggregate(Event~MismTreat + PhotoTreat + MotherID, survival_data, sum) # per mother
+survival_probs$samplesize <- aggregate(Info~MismTreat + PhotoTreat + MotherID, survival_data, length)$Info
+survival_probs$probs <- 100 - (survival_probs$Event/survival_probs$samplesize*100) # event = death
+head(survival_probs)
 
-surv_avg <- Rmisc::summarySE(surv_probs, measurevar="probs", groupvars=c("MismTreat")) # average of two photoperiod treatments
-surv_avg$samplesize <- aggregate(Info~MismTreat, d_surv, length)$Info
-surv_avg
+survival_avg <- Rmisc::summarySE(survival_probs, measurevar="probs", groupvars=c("MismTreat")) # average of two photoperiod treatments
+survival_avg$samplesize <- aggregate(Info~MismTreat, survival_data, length)$Info
+survival_avg
 
-raw_surv <- ggplot(data=surv_avg, aes(x=MismTreat, y=probs))+
+raw_survival_plot <- ggplot(data=survival_avg, aes(x=MismTreat, y=probs))+
   scale_colour_manual(values=c("grey27", "orangered2"))+ #"dodgerblue4"
   geom_jitter(data=surv_probs, aes(col=PhotoTreat), alpha=0.3, size=3, height=0.5, width=0.25)+
   geom_point(size=5, col="black") +
@@ -96,14 +100,14 @@ raw_surv <- ggplot(data=surv_avg, aes(x=MismTreat, y=probs))+
   theme(axis.title.y=element_text(size=18, vjust=2), axis.title.x=element_text(size=18, vjust=-0.5),
         axis.text=element_text(size=16), legend.text = element_text(size=16), legend.title=element_text(size=17)) +
   theme_cowplot()
-raw_surv 
+raw_survival_plot
 # ggsave(filename="_results/Survival_raw.png", plot=raw_surv , device="png", width=200, height=150, units="mm", dpi="print")
 
 
 
 # Fit binomial model --------------------------------------------------------------------------
 
-head(d_surv) # test if probability of survival differs between treatments
+head(survival_data) # test if probability of survival differs between treatments
 
 glm1 <- glmer(Event ~ (MismTreat1 + MismTreat2)*PhotoTreat + (1|MotherID), family=binomial, data=d_surv,
               na.action="na.fail", control=glmerControl(calc.derivs=F)) # helps convergence
@@ -123,7 +127,7 @@ glm_res <- summary(glm_final)$coefficients %>% as.data.frame
 # write.csv(rbind(anova1, anova2), file="_results/anova_Surv_glmer.csv", row.names=T)
 
 # Get predictions ####
-glm.pred <- d_surv[!duplicated(d_surv[,c("MotherID", "Treatment")]),] # each replicate assigned same prediction, so remove duplicates
+glm.pred <- survival_data[!duplicated(survival_data[,c("MotherID", "Treatment")]),] # each replicate assigned same prediction, so remove duplicates
 glm.pred$pred <- predict(glm_final, newdata=glm.pred, type="response") # predictions are probability of dying now
 glm.pred$survprob <- (1-glm.pred$pred)
 aggregate(survprob~MismTreat, data=glm.pred, mean) # peak at Day2
@@ -131,22 +135,22 @@ glm.pred$rel <- glm.pred$survprob/mean(filter(glm.pred, MismTreat==1)$survprob) 
 head(glm.pred)
 
 # Visualize predictions ####
-pred <- Rmisc::summarySE(glm.pred, measurevar="survprob", groupvars=c("MismTreat")) # average of two photoperiod treatments
-pred$samplesize <- aggregate(CaterpillarID~MismTreat, data=d_surv, length)$CaterpillarID
+predicted_survival <- Rmisc::summarySE(glm.pred, measurevar="survprob", groupvars=c("MismTreat")) # average of two photoperiod treatments
+predicted_survival$samplesize <- aggregate(CaterpillarID~MismTreat, data=survival_data, length)$CaterpillarID
 
 # Add predictions to raw data figure
-p_surv <- raw_surv + #geom_line(data=pred, aes(y=survprob*100)) +
+predicted_survival_plot <- raw_survival_plot + #geom_line(data=pred, aes(y=survprob*100)) +
   geom_smooth(data=pred, aes(y=survprob*100), se=F, col="red3")
-p_surv
+predicted_survival_plot
 # ggsave(filename="_results/Survival_wpred_rev.png", plot=p_surv, device="png", width=200, height=150, units="mm", dpi="print")
 
 rm(anova1, anova2, glm_res, glm1, glm2, pred, surv_probs, surv_avg, raw_surv) #cleanup
 
 
 # Pupation weight analysis --------------------------------------------------------------------
-head(d)
+head(cat_food_data)
 
-d_pupa <- d %>% mutate(PhotoTreat=gsub("(\\w+)Day.+","\\1",Treatment), MismTreat=gsub("\\w+(Day.+)","\\1",Treatment), PupaWeight=PupaWeight_ingrams*1000) %>%
+pupa_data <- cat_food_data %>% mutate(PhotoTreat=gsub("(\\w+)Day.+","\\1",Treatment), MismTreat=gsub("\\w+(Day.+)","\\1",Treatment), PupaWeight=PupaWeight_ingrams*1000) %>%
   select(ExperimentName, MotherID, Treatment, PhotoTreat, MismTreat, CaterpillarID, PupationAprilDay, PupaWeight) %>%
   filter(!is.na(PupationAprilDay)) %>%
   mutate(Treatment=as.factor(Treatment), PhotoTreat=as.factor(ifelse(PhotoTreat=="Chang", "Changing", "Constant")), MismTreatf=as.factor(MismTreat), MotherID=as.factor(MotherID)) %>%
@@ -156,19 +160,19 @@ d_pupa <- d %>% mutate(PhotoTreat=gsub("(\\w+)Day.+","\\1",Treatment), MismTreat
          MismTreat=as.numeric(gsub("Day(.+)","\\1",MismTreat))) %>%
   mutate(MismTreat1=MismTreat+5, # no negatives
          MismTreat2=(MismTreat+5)^2) # squared term to add in model
-head(d_pupa) # 346 individuals survived until pupation
-nrow(d_pupa)/nrow(d)*100 # ~35%
+head(pupa_data) # 346 individuals survived until pupation
+nrow(pupa_data)/nrow(d)*100 # ~35%
 
 
 # Visualize ####
-weight <- Rmisc::summarySE(d_pupa, measurevar="PupaWeight", groupvars=c("MismTreat", "PhotoTreat"))
-weight$pos <- ifelse(is.na(weight$se)==T, 0, weight$se) # position of sample size labels
-weight
+pupa_weight <- Rmisc::summarySE(pupa_data, measurevar="PupaWeight", groupvars=c("MismTreat", "PhotoTreat"))
+pupa_weight$pos <- ifelse(is.na(weight$se)==T, 0, weight$se) # position of sample size labels
+pupa_weight
 
-raw_weight <- ggplot(data=weight, aes(x=MismTreat, y=PupaWeight, col=PhotoTreat, fill=PhotoTreat))+
+raw_weight_figure <- ggplot(data=pupa_weight, aes(x=MismTreat, y=PupaWeight, col=PhotoTreat, fill=PhotoTreat))+
   scale_colour_manual(values=c("grey27", "orangered2"))+ #"dodgerblue4"
   scale_fill_manual(values=c("grey27", "orangered2"))+
-  geom_jitter(data=d_pupa, aes(col=PhotoTreat), alpha=0.3, size=3, height=0, width=0.25)+ #alpha=0.3, size=2, height=0, width=0.25
+  geom_jitter(data=pupa_data, aes(col=PhotoTreat), alpha=0.3, size=3, height=0, width=0.25)+ #alpha=0.3, size=2, height=0, width=0.25
   geom_errorbar(data=filter(weight, PhotoTreat=="Changing"), aes(ymax = PupaWeight+se, ymin=PupaWeight-se), width=0.3, col="black") +
   geom_errorbar(data=filter(weight, PhotoTreat=="Constant"), aes(ymax = PupaWeight+se, ymin=PupaWeight-se), width=0.3, col="orangered4") +
   geom_point(size=5, shape=21, col="black")+
@@ -179,14 +183,14 @@ raw_weight <- ggplot(data=weight, aes(x=MismTreat, y=PupaWeight, col=PhotoTreat,
   theme(axis.title.y=element_text(size=18, vjust=2), axis.title.x=element_text(size=18, vjust=-0.5),
         axis.text=element_text(size=16), legend.text = element_text(size=16), legend.title=element_text(size=17)) +
   theme_cowplot()
-raw_weight  
+raw_weight_figure
 # ggsave(filename="_results/PupWeight_raw.png", plot=raw_weight , device="png", width=200, height=150, units="mm", dpi="print")
 
 
 
 # Linear mixed model --------------------------------------------------------------------------
 
-lm1 <- lmer(PupaWeight ~ (MismTreat1 + MismTreat2)*PhotoTreat + (1|MotherID), data=d_pupa)
+lm1 <- lmer(PupaWeight ~ (MismTreat1 + MismTreat2)*PhotoTreat + (1|MotherID), data=pupa_data)
 anova1 <- anova(lm1) %>% as.data.frame() # interaction not significant
 anova1$mod <- "lm1"
 
@@ -226,11 +230,11 @@ pred1$samplesize <- weight$N
 pred1$pos <- ifelse(is.na(pred1$se)==T, 0, pred1$se) # position of sample size labels
 
 # add predictions to raw data figure
-p_weight <- raw_weight + #geom_line(data=pred1, aes(y=pred)) +
+pupa_weight_plot <- raw_weight_plot + #geom_line(data=pred1, aes(y=pred)) +
   geom_smooth(data=pred1, aes(y=pred, col=PhotoTreat), se=F, method=lm)+
   geom_text(data=filter(weight, PhotoTreat=="Changing"),aes(label=N, y=PupaWeight-pos-1.5), col="black", size=4, fontface="bold")+
   geom_text(data=filter(weight, PhotoTreat=="Constant"),aes(label=N, y=PupaWeight+pos+2.3), col="black", size=4, fontface="bold")
-p_weight
+pupa_weight_plot
 # ggsave(filename="_results/PupWeight_wpred_rev.png", plot=p_weight, device="png", width=200, height=150, units="mm", dpi="print")
 
 
@@ -257,9 +261,9 @@ head(lm.fit) # pred=predicted weight from lmer, only 154 observations
 
 # Fit curve to absolute fitness ####
 #-----------------------------------
-RelFit <- merge(glm.fit[,c("MotherID", "MismTreat", "survpred")], lm.fit[,c("MotherID", "MismTreat", "pred")], by=c("MotherID", "MismTreat"), all=T)
-colnames(RelFit)[c(3,4)] <- c("survpred", "pupwpred")
-RelFit$Fit <- RelFit$survpred*RelFit$pupwpred # multiply absolute values
+relative_fit <- merge(glm.fit[,c("MotherID", "MismTreat", "survpred")], lm.fit[,c("MotherID", "MismTreat", "pred")], by=c("MotherID", "MismTreat"), all=T)
+colnames(relative_fit)[c(3,4)] <- c("survpred", "pupwpred")
+relative_fit$Fit <- relative_fit$survpred*relative_fit$pupwpred # multiply absolute values
 head(RelFit) # can only do for 154 observations, clutches with >=1 caterpillar surviving until pupation
 table(RelFit$MismTreat, is.na(RelFit$Fit)) # for the other clutches, fitness = 0
 RelFit$Fit2 <- ifelse(is.na(RelFit$Fit)==T, 0, RelFit$Fit)
@@ -268,22 +272,22 @@ RelFit$Fit2 <- ifelse(is.na(RelFit$Fit)==T, 0, RelFit$Fit)
 loess_mod <- loess(Fit2~ -1 + MismTreat,  data=RelFit) 
 summary(loess_mod)
 
-curve <- RelFit[!duplicated(RelFit[,c("MismTreat")]),] %>% select(MismTreat)
+curve <- relative_fit[!duplicated(RelFit[,c("MismTreat")]),] %>% select(MismTreat)
 curve$pred <- predict(loess_mod, newdata=curve)
 curve <- arrange(curve, MismTreat)
 curve # peak at day2
 curve$rel <- curve$pred/filter(curve, MismTreat==2)$pred # expressive relative to peak
 
-RelFit$rel <- RelFit$Fit2/mean(filter(RelFit, MismTreat==2)$Fit2)
+relative_fit$rel <- relative_fit$Fit2/mean(filter(relative_fit, MismTreat==2)$Fit2)
 
-RelFit_means <- Rmisc::summarySE(RelFit, measurevar="rel", groupvars=c("MismTreat"))
+relative_fit_means <- Rmisc::summarySE(RelFit, measurevar="rel", groupvars=c("MismTreat"))
 #RelFit_means$samplesize <- aggregate(MotherID~MismTreat, data=d_pupa, length)$MotherID # number of caterpillars curve is based on
-RelFit_means$samplesize <- aggregate(MotherID~MismTreat, data=d_surv, length)$MotherID # number of caterpillars curve is based on = all
-RelFit_means$curve <- curve$rel
-head(RelFit_means)
+relative_fit_means$samplesize <- aggregate(MotherID~MismTreat, data=d_surv, length)$MotherID # number of caterpillars curve is based on = all
+relative_fit_means$curve <- curve$rel
+head(relative_fit_means)
 # write.csv(RelFit_means, file="_results/RelFitness_rev.csv", row.names=F)
 
-p_relfit <- ggplot(data=RelFit, aes(x=MismTreat, y=rel)) +
+relative_fit_plot <- ggplot(data=relative_fit, aes(x=MismTreat, y=rel)) +
   geom_jitter(size=3, alpha=0.4, height=0, width=0.25, shape=21, fill="dodgerblue2", col="dodgerblue4")+
   geom_point(data=RelFit_means, size=5) +
   geom_errorbar(data=RelFit_means, aes(ymax = rel+se, ymin=rel-se), width=0.3) +
@@ -297,7 +301,7 @@ p_relfit <- ggplot(data=RelFit, aes(x=MismTreat, y=rel)) +
   theme(axis.title.y=element_text(size=18, vjust=2), axis.title.x=element_text(size=18, vjust=-0.5),
         axis.text=element_text(size=16), legend.text = element_text(size=16), legend.title=element_text(size=17)) +
   theme_cowplot()
-p_relfit
+relative_fit_plot
 # ggsave(filename="_results/FitnessCurve_rev.png", plot=p_relfit, device="png", width=200, height=150, units="mm", dpi="print")
 
 
